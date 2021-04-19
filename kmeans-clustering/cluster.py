bc1c3a236b145a69c1680a1072b6e8789d862b93
Leo <zhanzhikai2015@gmail.com>
2021-04-19 13:13:51 -0400

    added finding k automatically

--- kmeans-clustering/cluster.py
+++ kmeans-clustering/cluster.py
@@ -3,6 +3,44 @@ from yellowbrick.cluster import KElbowVisualizer
 import numpy as np
 import csv
 
+
+import math
+import matplotlib.pylab as plt
+# import numpy as np
+# import seaborn as sns
+from sklearn.cluster import KMeans
+import warnings
+from pylab import rcParams
+
+
+def calc_distance(x1, y1, a, b, c):
+	d = abs((a * x1 + b * y1 + c)) / (math.sqrt(a * a + b * b))
+	return d
+
+def find_k(data):
+	dist_points_from_cluster_centre = []
+	K = range(2, 30)
+	for no_of_clusters in K:
+		k_model = KMeans(n_clusters=no_of_clusters)
+		k_model.fit(data)
+		dist_points_from_cluster_centre.append(k_model.inertia_)
+
+	a = dist_points_from_cluster_centre[0] - dist_points_from_cluster_centre[-1]
+	b = K[-1] - K[0]
+	c1 = K[0] * dist_points_from_cluster_centre[-1]
+	c2 = K[-1] * dist_points_from_cluster_centre[0]
+	c = c1 - c2
+
+	distance_of_points_from_line = []
+	for k in range(len(dist_points_from_cluster_centre)):
+		distance_of_points_from_line.append(
+			calc_distance(K[k], dist_points_from_cluster_centre[k], a, b, c))
+	
+	k = distance_of_points_from_line.index(max(distance_of_points_from_line)) + 1
+	
+	return k
+	
+
 def generate_clusteringInfo(filePath):
 	results = []
 
@@ -18,15 +56,19 @@ def generate_clusteringInfo(filePath):
 
 	allImages = np.array(results)
 
+	k = find_k(allImages)
+
+	print(k)
+
 	# # code for finding k
-	# model = KMeans()
+	model = KMeans()
 
-	# visualizer = KElbowVisualizer(model, k=(1,30))
+	visualizer = KElbowVisualizer(model, k=(1,30))
 
-	# visualizer.fit(allImages)        # Fit the data to the visualizer
-	# visualizer.show()        # Finalize and render the figure
+	visualizer.fit(allImages)        # Fit the data to the visualizer
+	visualizer.show()        # Finalize and render the figure
 
-	kmeans = KMeans(n_clusters=5, n_init = 100, random_state=0).fit(allImages)
+	kmeans = KMeans(n_clusters=k, n_init = 100, random_state=0).fit(allImages)
 
 	labels = kmeans.labels_
 
@@ -53,6 +95,6 @@ def generate_clusteringInfo(filePath):
 	output.close()
 
 
-# generate_clusteringInfo("../basic/index.csv")
+generate_clusteringInfo("../basic/index.csv")
 
 generate_clusteringInfo("../cnn-classifier/index.csv")
